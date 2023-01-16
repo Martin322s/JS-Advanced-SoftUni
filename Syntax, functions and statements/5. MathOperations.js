bc273756fb18c7d1b25ba79e1071cd3c7a110419
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-16 17:25:25 +0200

    js lab1 ex5

---
+++ Syntax, functions and statements/5. MathOperations.js
@@ -0,0 +1,22 @@
+function mathOperations(numOne, numTwo, operator) {
+
+    let result = 0;
+
+    switch (operator) {
+        case '+': result = numOne + numTwo;
+            break;
+        case '-': result = numOne - numTwo;
+            break;
+        case '*': result = numOne * numTwo;
+            break;
+        case '/': result = numOne / numTwo;
+            break;
+        case '**': result = numOne ** numTwo;
+            break;
+        case '%': result = numOne % numTwo;
+            break;
+    }
+
+    console.log(result);
+}
+mathOperations(3, 5.5, '*');
