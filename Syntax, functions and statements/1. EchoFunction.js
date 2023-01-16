2cb18bb649c4cb33baa4e152beff18598b97d703
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-16 17:24:06 +0200

    js lab1 ex1

---
+++ Syntax, functions and statements/1. EchoFunction.js
@@ -0,0 +1,9 @@
+function echoFunction(stringParam) {
+
+    let strLength = stringParam.length;
+
+    console.log(strLength);
+    console.log(stringParam);
+}
+echoFunction('Hello, JavaScript!');
+echoFunction('strings are easy');
