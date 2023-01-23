d552ee4f39b2cf9c07b0784305652a9efa0fffb8
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-23 15:17:45 +0200

    ex2 ex8

---
+++ Arrays and Nested arrays/Exercise/8. SortByTwoCriteria.js
@@ -0,0 +1,13 @@
+function sorting(input) {
+
+    input.sort(twoCriteriaSort);
+    input.forEach(el => console.log(el));
+
+    function twoCriteriaSort(cur, next) {
+        if (cur.length === next.length) {
+            return cur.localeCompare(next);
+        }
+        return cur.length - next.length;
+    }
+}
+sorting();
