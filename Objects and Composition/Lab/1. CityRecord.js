0fd24b53877b6d356e11e8262a867723411137ec
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-24 20:15:32 +0200

    lab3 ex1

---
+++ Objects and Composition/Lab/1. CityRecord.js
@@ -0,0 +1,11 @@
+function cityRecord(name, population, treasury) {
+
+    const obj = {
+        name,
+        population,
+        treasury
+    };
+
+    return obj;
+}
+cityRecord('Tortuga', 7000, 15000);
