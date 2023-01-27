eb954d44cb4bcce43e82199b9fe89884d236429d
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-27 18:44:01 +0200

    ex3 ex2

---
+++ Objects and Composition/Exercise/2. ConstructionCrew.js
@@ -0,0 +1,15 @@
+function constructionCrew(object) {
+
+    if (object.dizziness) {
+        object.levelOfHydrated = 0.1 * object.weight * object.experience;
+        object.dizziness = false;
+    }
+
+    return object;
+}
+constructionCrew({
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+});
