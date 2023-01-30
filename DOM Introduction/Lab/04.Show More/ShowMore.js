ed982c5413c5cf13353d9e57aa372ecb19497e0e
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-01-30 17:12:42 +0200

    lab4 ex4

---
+++ DOM Introduction/Lab/04.Show More/ShowMore.js
@@ -0,0 +1,8 @@
+function showText() {
+
+    const spanElement = document.getElementById('text');
+    const hrefElement = document.getElementById('more');
+
+    spanElement.style.display = 'inline';
+    hrefElement.style.display = 'none';
+}
