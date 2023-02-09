5d8f4f99506e081cd56e301335604b0fd3da45d8
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-09 14:50:47 +0200

    lab6 ex4

---
+++ Advanced Functions/Lab/04. FilterEmployees.js
@@ -0,0 +1,27 @@
+function solve(string, criteria) {
+
+    let data;
+    let propName, propValue;
+    const isNotAll = criteria !== 'all';
+
+    if (isNotAll) {
+        [propName, propValue] = criteria.split('-');
+
+        if (!propValue || !propValue) {
+            throw new Error('Criteria are in wrong format! Ivalid criteria!');
+        }
+    }
+
+    try {
+        data = JSON.parse(string);
+    } catch (error) {
+        data = [];
+    }
+
+    data.filter(function (i) {
+        return isNotAll ? i[propName] === propValue : true;
+    }).forEach((item, index) => {
+        console.log(`${index}. ${item.first_name} ${item.last_name} - ${item.email}`);
+    });
+}
+solve();
