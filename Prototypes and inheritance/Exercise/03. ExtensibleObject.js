8bd667a4e363c0a4ef136f18a91654f1baca8dbf
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-16 23:03:00 +0200

    lab8 ex3

---
+++ Prototypes and inheritance/Exercise/03. ExtensibleObject.js
@@ -0,0 +1,15 @@
+function extensibleObject() {
+    const proto = Object.getPrototypeOf(this);
+    this.extend = function (obj) {
+        for (const key in obj) {
+            const value = obj[key]
+            if (typeof value === 'function') {
+                proto[key] = value;
+            } else {
+                this[key] = value;
+            }
+        }
+    }
+
+    return this;
+}
