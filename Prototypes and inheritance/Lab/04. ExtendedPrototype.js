a2ae057902d000eebdc7bb40345c4ac72638828d
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-16 23:01:35 +0200

    lab8 ex4

---
+++ Prototypes and inheritance/Lab/04. ExtendedPrototype.js
@@ -0,0 +1,48 @@
+class Person {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
+ 
+    toString() {
+        let className = this.constructor.name;
+        return `${className} (name: ${this.name}, email: ${this.email})`;
+    }
+}
+ 
+class Teacher extends Person {
+    constructor(teacherName, teacherEmail, subject) {
+        super(teacherName, teacherEmail);
+        this.subject = subject;
+    }
+ 
+    toString() {
+        let base = super.toString().slice(0, -1);
+        return `${base}, subject: ${this.subject})`;
+    }
+}
+ 
+class Student extends Person {
+    constructor(studentName, studentEmail, course) {
+        super(studentName, studentEmail);
+        this.course = course;
+    }
+ 
+    toString() {
+        let base = super.toString().slice(0, -1);
+        return `${base}, course: ${this.course})`;
+    }
+}
+
+function extendPrototype(classDefinition) {
+
+
+    classDefinition.prototype.species = 'Human';
+
+    classDefinition.prototype.toSpeciesString = function () {
+        return `I am a ${this.species}. ${classDefinition.toString()}`;
+    } 
+}
+
+let p = new Person('Peter', 'peter@abv.bg');
+console.log(extendPrototype(Person));
