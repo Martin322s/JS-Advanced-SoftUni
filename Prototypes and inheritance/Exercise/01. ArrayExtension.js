7ba09ed8f29223e535f00448c131097ad1d6934f
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-02-16 23:02:33 +0200

    ex8 ex1

---
+++ Prototypes and inheritance/Exercise/01. ArrayExtension.js
@@ -0,0 +1,49 @@
+(function () {
+
+    Array.prototype.last = function () {
+        return this[this.length - 1];
+    };
+
+    Array.prototype.skip = function (n) {
+        let result = [];
+
+        for (let i = n; i < this.length; i++) {
+            result.push(this[i]);
+        }
+
+        return result;
+    }
+
+    Array.prototype.take = function (n) {
+        let result = [];
+
+        for (let i = 0; i < n; i++) {
+            result.push(this[i]);
+        }
+
+        return result;
+    }
+
+    Array.prototype.sum = function () {
+        let sum = 0;
+
+        for (let i = 0; i < this.length; i++) {
+            sum += Number(this[i]);
+        }
+
+        return sum;
+    }
+
+    Array.prototype.average = function () {
+        let sum = 0;
+        let average = 0;
+
+        for (let i = 0; i < this.length; i++) {
+            sum += Number(this[i]);
+            average = Math.ceil(sum / this.length);
+        }
+
+        return average;
+    }
+        
+})();
