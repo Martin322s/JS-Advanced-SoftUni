2fa94dba3e69b1028bd6be10eea59a964df396df
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-15 18:33:51 +0300

    js advanced - dom events

--- JS Advanced/05. DOM Manipulations/Exercise/05. Encode-Messages/app.js
+++ JS Advanced/05. DOM Manipulations/Exercise/05. Encode-Messages/app.js
@@ -1,31 +1,33 @@
 function encodeAndDecodeMessages() {
-
     document.getElementById('main').addEventListener('click', (e) => {
         if (e.target.tagName !== 'BUTTON') {
             return;
         }
 
-        let decodedMessages = document.getElementsByTagName('textarea')[0];
-        let encodedMessages = document.getElementsByTagName('textarea')[1];
+        let decodedMessage = document.getElementsByTagName('textarea')[0];
+        let encodedMessage = document.getElementsByTagName('textarea')[1];
 
         if (e.target.textContent.includes('Encode')) {
-            let message = decodedMessages.value;
+            let message = decodedMessage.value;
             let encoded = [];
 
             for (let i = 0; i < message.length; i++) {
                 let currSymbol = message.charCodeAt(i);
                 encoded.push(String.fromCharCode(currSymbol + 1));
             }
-            decodedMessages.value = '';
-            encodedMessages.value = encoded.join("");
+
+            decodedMessage.value = '';
+            encodedMessage.value = encoded.join('');
         } else if (e.target.textContent.includes('Decode')) {
-            let message = encodedMessages.value;
+            let message = encodedMessage.value;
             let decoded = [];
+
             for (let i = 0; i < message.length; i++) {
                 let currSymbol = message.charCodeAt(i);
                 decoded.push(String.fromCharCode(currSymbol - 1));
             }
-            encodedMessages.value = decoded.join("");
+
+            encodedMessage.value = decoded.join('');
         }
     });
 }
