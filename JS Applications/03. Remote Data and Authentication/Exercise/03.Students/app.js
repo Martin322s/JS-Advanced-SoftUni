fb0563f34228cb50d4db850ed8bcae96dfe3e51b
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-16 20:22:47 +0300

    js-apps remote data
    ex students

--- JS Applications/03. Remote Data and Authentication/Exercise/03.Students/app.js
+++ JS Applications/03. Remote Data and Authentication/Exercise/03.Students/app.js
@@ -1,1 +1,62 @@
-console.log('TODO...');
+const url = 'http://localhost:3030/jsonstore/collections/students';
+const form = document.getElementById('form');
+const tbody = document.querySelector('#results tbody');
+
+fetch(url)
+    .then(res => res.json())
+    .then(result => {
+        Object.values(result)
+            .forEach(x => {
+                let tr = createRow(x);
+                tbody.appendChild(tr);
+            });
+    });
+
+form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.currentTarget);
+    const firstName = formData.get('firstName');
+    const lastName = formData.get('lastName');
+    const facultyNumber = formData.get('facultyNumber');
+    const grade = formData.get('grade');
+
+    const data = { firstName, lastName, facultyNumber, grade: Number(grade) };
+
+    if (!Object.values(data).some(x => x === '')) {
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                let tr = createRow(result);
+                tbody.appendChild(tr);
+            });
+
+        ev.currentTarget.reset();
+    }
+});
+
+function createRow(data) {
+    let tr = document.createElement('tr');
+    let tdFirstName = document.createElement('td');
+    let tdLastName = document.createElement('td');
+    let tdFacultyNumber = document.createElement('td');
+    let tdGrade = document.createElement('td');
+
+    tdFirstName.textContent = data.firstName;
+    tdLastName.textContent = data.lastName;
+    tdFacultyNumber.textContent = data.facultyNumber;
+    tdGrade.textContent = data.grade;
+
+    tr.appendChild(tdFirstName);
+    tr.appendChild(tdLastName);
+    tr.appendChild(tdFacultyNumber);
+    tr.appendChild(tdGrade);
+
+    return tr;
+}
