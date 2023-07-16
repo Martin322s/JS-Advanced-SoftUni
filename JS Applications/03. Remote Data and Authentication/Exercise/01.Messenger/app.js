3d662d234be61b75697baf7cf5b5a1f58a600524
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-16 20:21:58 +0300

    js-apps remote data
    ex - messenger

--- JS Applications/03. Remote Data and Authentication/Exercise/01.Messenger/app.js
+++ JS Applications/03. Remote Data and Authentication/Exercise/01.Messenger/app.js
@@ -1,5 +1,34 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+    const username = document.querySelector('input[name="author"]');
+    const message = document.querySelector('input[name="content"]');
+    const sendBtn = document.getElementById('submit');
+    const messages = document.getElementById('messages');
+    const refreshBtn = document.getElementById('refresh');
+    const url = 'http://localhost:3030/jsonstore/messenger';
 
+    refreshBtn.addEventListener('click', () => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                const messagesArray = Object.values(data).map(x => `${x.author}: ${x.content}`);
+                messages.textContent = messagesArray.join('\n');
+            });
+    });
+
+    sendBtn.addEventListener('click', () => {
+        const author = username.value;
+        const content = message.value;
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ author, content })
+        });
+
+        username.value = '';
+        message.value = '';
+    });
+}
 attachEvents();
