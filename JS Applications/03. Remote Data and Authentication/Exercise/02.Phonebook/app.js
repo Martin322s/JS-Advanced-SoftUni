a1b74945cd23a048d364b519e2b4c8e09c5f1b99
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-16 20:22:25 +0300

    js-apps remote data
    ex phonebook

--- JS Applications/03. Remote Data and Authentication/Exercise/02.Phonebook/app.js
+++ JS Applications/03. Remote Data and Authentication/Exercise/02.Phonebook/app.js
@@ -1,5 +1,60 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+    const phoneBookList = document.getElementById('phonebook');
+    const loadBtn = document.getElementById('btnLoad');
+    const personInput = document.getElementById('person');
+    const phoneInput = document.getElementById('phone');
+    const createBtn = document.getElementById('btnCreate');
 
+    loadBtn.addEventListener('click', () => {
+        getAllPhones()
+            .then(data => {
+                phoneBookList.replaceChildren();
+
+                Object.values(data)
+                    .forEach(x => {
+                        let liElement = document.createElement('li');
+                        let deleteBtn = document.createElement('button');
+
+                        liElement.textContent = `${x.person}: ${x.phone}`;
+                        deleteBtn.textContent = 'Delete';
+
+                        liElement.appendChild(deleteBtn);
+                        phoneBookList.appendChild(liElement);
+
+                        deleteBtn.addEventListener('click', () => {
+                            fetch(`http://localhost:3030/jsonstore/phonebook/${x._id}`, {
+                                method: 'DELETE',
+                            })
+                                .then(res => res.json())
+                                .then(() => {
+                                    loadBtn.click();
+                                });
+                        });
+                    });
+            });
+    });
+
+    createBtn.addEventListener('click', () => {
+        if (personInput.value !== '' && phoneInput.value !== '') {
+            fetch(`http://localhost:3030/jsonstore/phonebook`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ person: personInput.value, phone: phoneInput.value })
+            })
+                .then(res => res.json())
+                .then(() => {
+                    loadBtn.click();
+                    personInput.value = '';
+                    phoneInput.value = '';
+                });
+        }
+    });
+
+    async function getAllPhones() {
+        const res = await fetch(`http://localhost:3030/jsonstore/phonebook`);
+        return await res.json();
+    }
+}
 attachEvents();
