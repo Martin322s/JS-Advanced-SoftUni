65656145108a3509a2535275847b6c1a5449f0b8
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-17 16:56:31 +0300

    js apps csr ex 5

---
+++ JS Applications/06. Client Side Rendering/Exercise/05.Table-Search/solution.js
@@ -0,0 +1,46 @@
+import { html, render } from '../../../node_modules/lit-html/lit-html.js';
+import * as api from './api.js';
+
+const input = document.querySelector('#searchField');
+const trs = document.getElementsByTagName('tr');
+
+document.querySelector('#searchBtn').addEventListener('click', onClick);
+
+const loadData = async () => {
+   const data = await api.getData();
+
+   const template = Object.values(data).map(renderProfile);
+   render(template, document.querySelector('.container tbody'));
+};
+
+const renderProfile = (profile) => {
+   return html`
+    <tr>
+      <td>${profile.firstName} ${profile.lastName}</td>
+      <td>${profile.email}</td>
+      <td>${profile.course}</td>
+    </tr>
+  `;
+};
+
+function onClick() {
+   const searchText = input.value.toLowerCase();
+
+   if (searchText == '') {
+      return;
+   }
+
+   Array.from(trs).forEach((tr) => {
+      const rowContent = tr.textContent.toLowerCase();
+
+      if (rowContent.includes(searchText)) {
+         tr.className = 'select';
+      } else {
+         tr.className = '';
+      }
+   });
+
+   input.value = '';
+}
+
+loadData();
