23cee31e1b4a422521a5a77ffec0d3b8d4892509
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-20 01:23:37 +0300

    login user api service call

--- JS Applications/09. Exam Preparation 1/src/services/authService.js
+++ JS Applications/09. Exam Preparation 1/src/services/authService.js
@@ -11,3 +11,15 @@ export const registerUser = async (userData) => {
 
     return await res.json();
 };
+
+export const loginUser = async (userData) => {
+    const res = await fetch(`${baseUrl}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userData)
+    });
+
+    return await res.json();
+};
