f36b610ddd2b1d40600d64fdad94326661fa472a
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-20 01:11:35 +0300

    create view template

--- JS Applications/09. Exam Preparation 1/src/views/createView.js
+++ JS Applications/09. Exam Preparation 1/src/views/createView.js
@@ -1,5 +1,40 @@
 import { html } from '../../node_modules/lit-html/lit-html.js';
 
 const createView = () => html`
-    
+    <section id="create">
+        <div class="form">
+            <h2>Add Fruit</h2>
+            <form class="create-form">
+                <input 
+                    type="text" 
+                    name="name" 
+                    id="name" 
+                    placeholder="Fruit Name" 
+                />
+                <input 
+                    type="text" 
+                    name="imageUrl" 
+                    id="Fruit-image" 
+                    placeholder="Fruit Image" 
+                />
+                <textarea 
+                    id="fruit-description" 
+                    name="description" 
+                    placeholder="Description" 
+                    rows="10"
+                    cols="50"
+                >
+                </textarea>
+                <textarea 
+                    id="fruit-nutrition" 
+                    name="nutrition" 
+                    placeholder="Nutrition" 
+                    rows="10"
+                    cols="50"
+                >
+                </textarea>
+                <button type="submit">Add Fruit</button>
+            </form>
+        </div>
+    </section>
 `;
