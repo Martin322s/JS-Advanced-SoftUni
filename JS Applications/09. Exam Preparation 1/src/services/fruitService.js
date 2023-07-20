468a47b5c460bfbb25b7bee268ca4aeaf1815572
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-21 00:35:15 +0300

    create fruit service call

--- JS Applications/09. Exam Preparation 1/src/services/fruitService.js
+++ JS Applications/09. Exam Preparation 1/src/services/fruitService.js
@@ -1,3 +1,16 @@
 const baseUrl = 'http://localhost:3030/data';
 
 export const getAll = () => fetch(`${baseUrl}/fruits?sortBy=_createdOn%20desc`).then(res => res.json());
+
+export const createFruit = async (fruitData, token) => {
+    const res = await fetch(`${baseUrl}/fruits`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': token
+        },
+        body: JSON.stringify(fruitData)
+    });
+
+    return await res.json();
+};
