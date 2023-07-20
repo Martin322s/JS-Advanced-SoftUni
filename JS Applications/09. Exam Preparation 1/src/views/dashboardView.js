36eb6ceabd1a137bb56378efc9b97e20a383aa58
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-21 00:53:38 +0300

    render description info

--- JS Applications/09. Exam Preparation 1/src/views/dashboardView.js
+++ JS Applications/09. Exam Preparation 1/src/views/dashboardView.js
@@ -1,36 +1,34 @@
 import { html, nothing } from '../../node_modules/lit-html/lit-html.js';
 import { getAll } from '../services/fruitService.js';
 
-const fruitTemplate = (fruit, accessToken) => html`
+const fruitTemplate = (fruit) => html`
     <div class="fruit">
         <img src=${fruit.imageUrl} alt="example1" />
         <h3 class="title">${fruit.name}</h3>
         <p class="description">
-            ${fruit.nutrition}
+            ${fruit.description}
         </p>
-        ${accessToken
-            ?
-            html`<a class="details-btn" href="/details/${fruit._id}">More Info</a>`
-            :
-            nothing
-        }
+            <a class="details-btn" href="/details/${fruit._id}">More Info</a>
     </div>
 `;
 
 const dashboardView = (allFruits, accessToken) => html`
     <h2>Fruits</h2>
-    <section id="dashboard">
+    
     ${allFruits.length > 0
         ?
-        allFruits.map(x => fruitTemplate(x, accessToken))
+        html`
+        <section id="dashboard">
+            ${allFruits.map(x => fruitTemplate(x, accessToken))}
+        </section>
+        `
         :
         html`<h2>No fruit info yet.</h2>`
     }
-    </section>
+    
 `;
 
 export const renderDashboard = async (ctx) => {
     const allFruits = await getAll();
-    const accessToken = JSON.parse(localStorage.getItem('user'))?.accessToken;
-    ctx.mainRender(dashboardView(allFruits, accessToken));
+    ctx.mainRender(dashboardView(allFruits));
 };
