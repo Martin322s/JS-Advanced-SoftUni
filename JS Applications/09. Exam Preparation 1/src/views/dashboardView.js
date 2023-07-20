1d054d2492f3aba7e44f59d94cccac19ac5c2d37
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-20 16:52:43 +0300

    render all fruits in the dashboard

--- JS Applications/09. Exam Preparation 1/src/views/dashboardView.js
+++ JS Applications/09. Exam Preparation 1/src/views/dashboardView.js
@@ -3,17 +3,12 @@ import { getAll } from '../services/fruitService.js';
 
 const fruitTemplate = (fruit) => html`
     <div class="fruit">
-        <img src="./images/fruit 1.png" alt="example1" />
-        <h3 class="title">Pineapple</h3>
+        <img src=${fruit.imageUrl} alt="example1" />
+        <h3 class="title">${fruit.name}</h3>
         <p class="description">
-            The pineapple is a tropical plant with an edible fruit.
-            It is the most economically significant plant in the family Bromeliaceae.The
-            pineapple is indigenous to South America. Pineapples grow as a small shrub, the individual 
-            flowers of the unpollinated plant fuse to form a multiple fruit. The plant is 
-            normally propagated from the offset produced at the top of the fruit,or 
-            from a side shoot, and typically matures within a year.
+            ${fruit.nutrition}
         </p>
-        <a class="details-btn" href="">More Info</a>
+        <a class="details-btn" href="/details/${fruit._id}">More Info</a>
     </div>
 `;
 
@@ -31,6 +26,5 @@ const dashboardView = (allFruits) => html`
 
 export const renderDashboard = async (ctx) => {
     const allFruits = await getAll();
-    console.log(allFruits);
     ctx.mainRender(dashboardView(allFruits));
 };
